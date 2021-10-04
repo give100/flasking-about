0880f424402d0da738f3efbcde48a840ce12248b
givingahundred <givingahundred@gmail.com>
2021-10-04 17:51:50 +0100

    Setup flask with database and ui
    Add database capability with postgresql as a docker image in docker-compose

---
+++ models.py
@@ -0,0 +1,34 @@
+from . import app,db
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/cars_api"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class CarsModel(db.Model):
+    __tablename__ = 'cars'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    model = db.Column(db.String())
+    doors = db.Column(db.Integer())
+
+    def __init__(self, name, model, doors):
+        self.name = name
+        self.model = model
+        self.doors = doors
+
+    def __repr__(self):
+        return f"<Car {self.name}>"
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, email):
+        self.email = email
