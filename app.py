d10d19665db2d3cd2ab3aed0e1299f31d58aa671
givingahundred <givingahundred@gmail.com>
2021-10-05 13:25:02 +0100

    Fix broken webpage due to import looping
    python was having an import loop issue.

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
-from models.user import User
 
-app = Flask(__name__)
-app.config.from_object("config.Config")
+
+def create_app(config_object="settings"):
+    """Create application factory, as explained here: http://flask.pocoo.org/docs/patterns/appfactories/.
+
+    :param config_object: The configuration object to use.
+    """
+    app = Flask(__name__)
+    app.config.from_object(config_object)
+    return app
+
+app = create_app()
 db = SQLAlchemy(app)
+
+from models import User
 
 @app.route('/')
 def home():
     return render_template('home.html', users=User.query.all())
+
